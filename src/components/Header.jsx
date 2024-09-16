ca9dc60400f089923df5121c743887b557e90def
Dudous <sampaioeeduardo36@gmail.com>
2024-09-16 15:11:38 -0300

    Header aceitável

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import style from './Header.module.css'
+
+export const Header = () => {
+
+    return(
+        <>
+            <nav>
+            <div className={style.navbar}>
+                <div className={style.name}>
+                    <a className={style.text} href="">Eduardo Sampaio</a>
+                </div>
+                <div className={style.sessenta}>
+                    <ul className={style.pageList}>
+                        <li className={style.page}><a className={style.text} href="#Home">Home</a></li>
+                        <li className={style.page}><a className={style.text} href="#Projects">Projects</a></li>
+                        <li className={style.page}><a className={style.text} href="">About</a></li>
+                        <li className={style.page}><a className={style.text} href="">Soft skills</a></li>
+                        <li className={style.page}><a className={style.text} href="">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+            </nav>
+        </>  
+    )
+  }
